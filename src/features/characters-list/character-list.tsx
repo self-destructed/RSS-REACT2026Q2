b66db82730d2b18e3afccfe72c9838f59448a03a
self-destructed <self.destructed.dan@gmail.com>
2026-04-28 15:21:02 +0300

    feat(Characters-list): add component

---
+++ src/features/characters-list/character-list.tsx
@@ -0,0 +1,25 @@
+import { Component, type ReactNode } from 'react';
+import CharacterCard from './character-card/character-card';
+import type { Character } from '../../services/api-interfaces';
+
+interface Props {
+  data: Character[];
+}
+
+class CharactersList extends Component<Props> {
+  public render(): ReactNode {
+    return (
+      <section className="mt-6 px-4 md:px-8">
+        <div className="mx-auto max-w-md sm:max-w-4xl lg:max-w-6xl">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
+            {this.props.data.map((item) => (
+              <CharacterCard key={item.id} data={item} />
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default CharactersList;
