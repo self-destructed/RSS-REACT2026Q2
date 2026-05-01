b45c64373c8b15aede157801ae1563ff5e19fe85
self-destructed <self.destructed.dan@gmail.com>
2026-05-02 01:44:25 +0300

    feat(ErrorTrigger): add component

---
+++ src/shared/ui/error/ErrorTrigger.tsx
@@ -0,0 +1,26 @@
+import { Component, type ReactNode } from 'react';
+
+class ErrorTrigger extends Component<object, { shouldError: boolean }> {
+  state = { shouldError: false };
+
+  private handleClick = (): void => {
+    this.setState({ shouldError: true });
+  };
+
+  public render(): ReactNode {
+    if (this.state.shouldError) {
+      throw new Error('💣 Boom! Error triggered by user');
+    }
+
+    return (
+      <button
+        onClick={this.handleClick}
+        className="rounded-md bg-red-600 px-3 py-1.5 text-sm font-medium text-white hover:bg-red-700"
+      >
+        💣 Trigger Error
+      </button>
+    );
+  }
+}
+
+export default ErrorTrigger;
--- src/shared/ui/error/index.ts
+++ src/shared/ui/error/index.ts
@@ -1,3 +1,4 @@
 export { default as ErrorDisplay } from './ErrorDisplay';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as ErrorTrigger } from './ErrorTrigger';
 export { default as ErrorFallback } from './ErrorFallback';
