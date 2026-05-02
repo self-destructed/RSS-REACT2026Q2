2dcba5531f24de8a4383a8818cebb43d86a0b2ca
self-destructed <self.destructed.dan@gmail.com>
2026-05-02 20:07:56 +0300

    fix(CharacterList): remove unnecessary markup

--- src/features/characters/ui/CharacterList.tsx
+++ src/features/characters/ui/CharacterList.tsx
@@ -7,17 +7,15 @@ interface Props {
 }
 
 class CharactersList extends Component<Props> {
-  public render(): ReactNode {
+  render(): ReactNode {
     return (
-      <section className="mt-6 px-4 md:px-8">
-        <div className="mx-auto max-w-md sm:max-w-4xl lg:max-w-6xl">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
-            {this.props.data.map((item) => (
-              <CharacterCard key={item.id} data={item} />
-            ))}
-          </div>
-        </div>
-      </section>
+      <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
+        {this.props.data.map((item) => (
+          <li key={item.id}>
+            <CharacterCard data={item} />
+          </li>
+        ))}
+      </ul>
     );
   }
 }
