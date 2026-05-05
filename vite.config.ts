48137f0296b8a726dd9060873af769a1f8bf9293
self-destructed <self.destructed.dan@gmail.com>
2026-05-05 23:57:21 +0300

    chore: add vitest, testing library and setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -6,4 +6,33 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/RSS-REACT2026Q2/',
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true, // Allows using describe/it/expect without importing
+    environment: 'jsdom', // Simulates the browser DOM
+    setupFiles: ['./src/__tests__/setupTests.ts'], // Global setup file
+    css: true, // Processes CSS in tests (useful for CSS modules)
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/main.tsx',
+        'src/**/*.d.ts',
+        'src/**/*.stories.{ts,tsx}',
+        'src/types/**',
+      ],
+
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 75,
+        statements: 80,
+        // statements: 80,
+        // branches: 50,
+        // functions: 50,
+        // lines: 50,
+      },
+    },
+  },
 });
