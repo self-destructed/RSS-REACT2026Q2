fae29e5ea4c5d6e4084038d668506591e76030dc
self-destructed <self.destructed.dan@gmail.com>
2026-05-12 15:21:26 +0300

    refactor(Layout): convert to functional component

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,17 +1,13 @@
-import { Component, type ReactNode } from 'react';
+import { type ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
 }
 
-class Layout extends Component<Props> {
-  render(): ReactNode {
-    return (
-      <div className="min-h-screen bg-slate-200 dark:bg-neutral-950">
-        {this.props.children}
-      </div>
-    );
-  }
+export default function Layout({ children }: Props) {
+  return (
+    <div className="min-h-screen bg-slate-200 dark:bg-neutral-950">
+      {children}
+    </div>
+  );
 }
-
-export default Layout;
