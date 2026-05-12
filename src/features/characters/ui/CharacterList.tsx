78c3c777540ceb1827bcd7a12e86d9cb10cee319
self-destructed <self.destructed.dan@gmail.com>
2026-05-12 17:36:24 +0300

    refactor(CharacterList): convert to functional component

--- src/features/characters/ui/CharacterList.tsx
+++ src/features/characters/ui/CharacterList.tsx
@@ -1,23 +1,17 @@
-import { Component, type ReactNode } from 'react';
 import CharacterCard from './CharacterCard';
 import type { Character } from '../../../shared/api/types';
 
 interface Props {
   data: Character[];
 }
-
-class CharactersList extends Component<Props> {
-  render(): ReactNode {
-    return (
-      <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
-        {this.props.data.map((item) => (
-          <li key={item.id}>
-            <CharacterCard data={item} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
+export default function CharacterList({ data }: Props) {
+  return (
+    <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
+      {data.map((item) => (
+        <li key={item.id}>
+          <CharacterCard data={item} />
+        </li>
+      ))}
+    </ul>
+  );
 }
-
-export default CharactersList;
