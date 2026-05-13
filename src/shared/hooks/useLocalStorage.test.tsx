4f0fbb02fbe6beeefb85ea9c14761dc84ed7ee26
self-destructed <self.destructed.dan@gmail.com>
2026-05-13 17:48:18 +0300

    test(useLocalStorage): add tests

---
+++ src/shared/hooks/useLocalStorage.test.tsx
@@ -0,0 +1,90 @@
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import useLocalStorage from './useLocalStorage';
+import { createLocalStorageMock } from '../api/__mocks__/local-storage';
+import { act, renderHook } from '@testing-library/react';
+
+describe('useLocalStorage', () => {
+  beforeEach(() => {
+    const mock = createLocalStorageMock();
+    vi.stubGlobal('localStorage', mock);
+  });
+
+  afterAll(() => {
+    vi.unstubAllGlobals();
+  });
+
+  describe('initialization', () => {
+    it('should return initial value when localStorage is empty', () => {
+      const initialValue = 'initialValue';
+      const { result } = renderHook(() =>
+        useLocalStorage('testKey', initialValue)
+      );
+      const [value] = result.current;
+
+      expect(value).toBe(initialValue);
+    });
+
+    it('should return value from localStorage if it exists', () => {
+      const storedValue = 'storedValue';
+      localStorage.setItem('testKey', JSON.stringify(storedValue));
+
+      const { result } = renderHook(() =>
+        useLocalStorage('testKey', 'initialValue')
+      );
+      const [value] = result.current;
+
+      expect(value).toBe(storedValue);
+    });
+
+    it('should return initial value if localStorage is unavailable', () => {
+      vi.stubGlobal('localStorage', undefined);
+      const { result } = renderHook(() =>
+        useLocalStorage('testKey', 'initialValue')
+      );
+      const [value] = result.current;
+
+      expect(value).toBe('initialValue');
+    });
+  });
+
+  describe('setValue', () => {
+    it('should update localStorage when the value changes', () => {
+      const { result } = renderHook(() =>
+        useLocalStorage('testKey', 'initialValue')
+      );
+      const [, setValue] = result.current;
+      const newValue = 'newValue';
+
+      act(() => {
+        setValue(newValue);
+      });
+      const storedValue = JSON.parse(localStorage.getItem('testKey') || '');
+
+      expect(storedValue).toBe(newValue);
+    });
+
+    it('should handle function updater', () => {
+      const { result } = renderHook(() => useLocalStorage('testKey', 0));
+      const [, setValue] = result.current;
+
+      act(() => {
+        setValue((prev) => prev + 1);
+      });
+      const storedValue = JSON.parse(localStorage.getItem('testKey') || '123');
+
+      expect(storedValue).toBe(1);
+    });
+  });
+
+  describe('error handling', () => {
+    it('should return initial value if JSON.parse fails', () => {
+      localStorage.setItem('testKey', 'invalidJson{');
+      const { result } = renderHook(() =>
+        useLocalStorage('testKey', 'initialValue')
+      );
+      const [value] = result.current;
+
+      expect(value).toBe('initialValue');
+    });
+  });
+});
