090b3a206a4f30bae9695df476429b5e48a910b6
self-destructed <self.destructed.dan@gmail.com>
2026-05-13 21:58:44 +0300

    feat(App): add react-router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import { Component, type ReactNode } from 'react';
 import { ErrorBoundary, ErrorFallback, ErrorTrigger } from '../shared/ui/error';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import CharactersPage from './pages/characters';
 
 class App extends Component {
   render(): ReactNode {
     return (
       <ErrorBoundary fallback={(reset) => <ErrorFallback reset={reset} />}>
-        <CharactersPage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/RSS-REACT2026Q2/" element={<CharactersPage />} />
+          </Routes>
+        </BrowserRouter>
         <div className="fixed right-4 bottom-4 z-50">
           <ErrorTrigger />
         </div>
