3e69d54b4bd81849843d9c14c0dc1937cf5cc13b
self-destructed <self.destructed.dan@gmail.com>
2026-05-15 23:46:21 +0300

    feat(AboutPage): add component, path, route, link

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router';
 import { PATHS } from '../shared/constants/paths';
 import CharactersPage from './pages/characters';
+import AboutPage from './pages/about';
 import ErrorPage from './pages/error';
 
 export function AppRoutes() {
@@ -8,6 +9,7 @@ export function AppRoutes() {
     <Routes>
       <Route index element={<Navigate to={PATHS.CHARACTERS} replace />} />
       <Route path={PATHS.CHARACTERS} element={<CharactersPage />} />
+      <Route path={PATHS.ABOUT} element={<AboutPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
--- src/shared/ui/navbar/Navbar.tsx
+++ src/shared/ui/navbar/Navbar.tsx
@@ -22,6 +22,14 @@ export default function Navbar() {
             <li className="mb-4 ps-2 lg:mb-0 lg:ps-0 lg:pe-1">
               <Link
                 className="p-0 text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none lg:px-2 dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+                to={PATHS.ABOUT}
+              >
+                About
+              </Link>
+            </li>
+            <li className="mb-4 ps-2 lg:mb-0 lg:ps-0 lg:pe-1">
+              <Link
+                className="p-0 text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none lg:px-2 dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
                 to={PATHS.ERROR}
               >
                 Error
