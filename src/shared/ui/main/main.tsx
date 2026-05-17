ba44f46354773b670ff7ba84fd1993b27b3f3cf6
self-destructed <self.destructed.dan@gmail.com>
2026-05-18 00:40:15 +0300

    refactor(Main): update due to new config

--- src/shared/ui/main/main.tsx
+++ src/shared/ui/main/main.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: ReactNode;
 }
 
-export default function Main({ children }: Props) {
+export default function Main({ children }: Props): React.JSX.Element {
   return (
     <main>
       <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 sm:py-10 lg:px-8 lg:py-12">
