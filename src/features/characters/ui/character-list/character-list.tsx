6ad4bc444e7389d3aab41ff3446dcbaf4b60432a
self-destructed <self.destructed.dan@gmail.com>
2026-05-18 00:18:00 +0300

    refactor(CharacterList): update due to new config

--- src/features/characters/ui/character-list/character-list.tsx
+++ src/features/characters/ui/character-list/character-list.tsx
@@ -5,16 +5,27 @@ interface Props {
   onSelect?: (id: number) => void;
   data: Character[];
 }
-export default function CharacterList({ data, onSelect }: Props) {
+export default function CharacterList({
+  data,
+  onSelect,
+}: Props): React.JSX.Element {
   return (
     <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
       {data.map((item) => (
-        <li
-          key={item.id}
-          onClick={() => onSelect?.(item.id)}
-          className="cursor-pointer"
-        >
-          <CharacterCard data={item} />
+        <li key={item.id}>
+          <div
+            role="button"
+            tabIndex={0}
+            onClick={() => onSelect?.(item.id)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                onSelect?.(item.id);
+              }
+            }}
+            className="cursor-pointer"
+          >
+            <CharacterCard data={item} />
+          </div>
         </li>
       ))}
     </ul>
