21da93a1a8c164aa7ede3954cf0d2e1ecf41b21d
self-destructed <self.destructed.dan@gmail.com>
2026-05-17 23:04:18 +0300

    feat(useCharacter, useCharacters): add hook

---
+++ src/features/characters/hooks/use-character/use-character.ts
@@ -0,0 +1,31 @@
+import type {
+  Character,
+  CharacterFilter,
+  Info,
+} from '../../../shared/api/types';
+import useFetch from '../../../shared/hooks/useFetch';
+import { buildQueryString } from '../../../shared/utils';
+
+const BASE_URL = 'https://rickandmortyapi.com/api';
+const CHARACTER_ENDPOINT = '/character';
+
+type LoadingState<T> =
+  | { status: 'idle' }
+  | { status: 'loading' }
+  | { status: 'success'; data: T }
+  | { status: 'error'; error: Error };
+
+export function useCharacters(
+  filters?: CharacterFilter
+): LoadingState<Info<Character[]>> {
+  const params = filters as
+    | Record<string, string | number | null | undefined>
+    | undefined;
+  const url = `${BASE_URL}${CHARACTER_ENDPOINT}?${buildQueryString(params)}`;
+  return useFetch<Info<Character[]>>(url);
+}
+
+export function useCharacter(id: number | null): LoadingState<Character> {
+  const url = id ? `${BASE_URL}${CHARACTER_ENDPOINT}/${id}` : null;
+  return useFetch<Character>(url);
+}
