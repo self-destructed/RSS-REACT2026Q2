1e796a3a011ebb7f3d1354b54936bbbc78e85f5b
self-destructed <self.destructed.dan@gmail.com>
2026-05-18 00:34:07 +0300

    refactor(ErrorDisplay): update due to new config

--- src/shared/ui/error/error-display.tsx
+++ src/shared/ui/error/error-display.tsx
@@ -2,7 +2,7 @@ interface Props {
   message: string;
 }
 
-export default function ErrorDisplay({ message }: Props) {
+export default function ErrorDisplay({ message }: Props): React.JSX.Element {
   return (
     <div className="rounded-lg bg-red-50 p-4 text-center text-red-700 dark:bg-red-950/40 dark:text-red-300">
       Error: {message}
