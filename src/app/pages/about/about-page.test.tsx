fa71758249c3c35cf502266d187235396f604f51
self-destructed <self.destructed.dan@gmail.com>
2026-05-22 04:50:27 +0300

    test(AboutPage): update test

--- src/app/pages/about/about-page.test.tsx
+++ src/app/pages/about/about-page.test.tsx
@@ -1,40 +1,35 @@
 import { describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router";
 import AboutPage from "./about-page";
 
-const renderWithRouter = () => {
-  return render(
-    <BrowserRouter>
-      <AboutPage />
-    </BrowserRouter>,
-  );
+const renderComponent = () => {
+  return render(<AboutPage />);
 };
 
 describe("AboutPage", () => {
   it("renders About heading", () => {
-    const { getByRole } = renderWithRouter();
+    const { getByRole } = renderComponent();
     const heading = getByRole("heading", { name: /about/i });
 
     expect(heading).toBeInTheDocument();
   });
 
   it("renders RS School info", () => {
-    const { getByText } = renderWithRouter();
+    const { getByText } = renderComponent();
     const infoText = getByText(/touch some grass/i);
 
     expect(infoText).toBeInTheDocument();
   });
 
   it("renders link to RS School course", () => {
-    const { getByRole } = renderWithRouter();
+    const { getByRole } = renderComponent();
     const link = getByRole("link", { name: /rs school/i });
 
     expect(link).toBeInTheDocument();
   });
 
   it("has correct link href", () => {
-    const { getByRole } = renderWithRouter();
+    const { getByRole } = renderComponent();
     const link = getByRole("link", { name: /rs school/i });
 
     expect(link).toHaveAttribute("href", "https://rs.school/courses/reactjs");
