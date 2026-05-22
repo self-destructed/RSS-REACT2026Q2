1cb0fb703e70d8de8f722812153ab02dad75352c
self-destructed <self.destructed.dan@gmail.com>
2026-05-22 04:11:32 +0300

    feat(App): add ThemeProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,19 @@
 import { ErrorBoundary, ErrorFallback, ErrorTrigger } from "@shared/ui/error";
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./AppRoutes";
+import { ThemeProvider } from "@shared/context";
 
 export default function App(): React.JSX.Element {
   return (
-    <ErrorBoundary fallback={(reset) => <ErrorFallback reset={reset} />}>
-      <BrowserRouter basename="/RSS-REACT2026Q2/">
-        <AppRoutes />
-      </BrowserRouter>
-      <div className="fixed right-4 bottom-4 z-50">
-        <ErrorTrigger />
-      </div>
-    </ErrorBoundary>
+    <ThemeProvider>
+      <ErrorBoundary fallback={(reset) => <ErrorFallback reset={reset} />}>
+        <BrowserRouter basename="/RSS-REACT2026Q2/">
+          <AppRoutes />
+        </BrowserRouter>
+        <div className="fixed right-4 bottom-4 z-50">
+          <ErrorTrigger />
+        </div>
+      </ErrorBoundary>
+    </ThemeProvider>
   );
 }
