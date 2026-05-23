94e79b590db11dd561a09ab5a3a739deb6305d14
self-destructed <self.destructed.dan@gmail.com>
2026-05-23 22:47:58 +0300

    fix(Layout): full-height grid layout

--- src/shared/ui/layout/layout.tsx
+++ src/shared/ui/layout/layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function Layout({ children }: Props): React.JSX.Element {
   return (
-    <div className="min-h-screen bg-slate-200 dark:bg-neutral-950">
+    <div className="grid min-h-screen grid-rows-[min-content_1fr_auto] bg-slate-200 dark:bg-neutral-950">
       <Header />
       {children}
     </div>
