605a7bdc13832931b28103944a880790fd2b06b6
self-destructed <self.destructed.dan@gmail.com>
2026-05-23 06:29:44 +0300

    refactor(App): change ErrorTrigger position

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,7 +10,7 @@ export function App(): React.JSX.Element {
         <BrowserRouter basename="/RSS-REACT2026Q2/">
           <AppRoutes />
         </BrowserRouter>
-        <div className="fixed right-4 bottom-4 z-50">
+        <div className="fixed left-0 top-1/2 z-50 origin-top-left -translate-y-1/2 -rotate-90">
           <ErrorTrigger />
         </div>
       </ErrorBoundary>
