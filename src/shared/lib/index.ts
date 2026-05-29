3faa8cf62bdfae46eefc77b6a70e932c8b996c13
self-destructed <self.destructed.dan@gmail.com>
2026-05-29 13:19:54 +0300

    del(lib): delete useFetch

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,8 +1,3 @@
 export { downloadFile, downloadCSV, escapeCSVValue, toCSV } from "./csv";
 export { buildQueryString, updateSearchParams } from "./url-params";
-export {
-  useFetch,
-  useLocalStorage,
-  useTheme,
-  type LoadingState,
-} from "./hooks";
+export { useLocalStorage, useTheme } from "./hooks";
