88f21eae9efc9cbc610921330a2a84f584939079
self-destructed <self.destructed.dan@gmail.com>
2026-06-04 20:41:49 +0300

    feat(createValidFormData): add function to create valid form data with optional overrides

---
+++ src/lib/createValidFormData.ts
@@ -0,0 +1,16 @@
+import type { FormValues } from "./validationSchemas";
+
+export function createValidFormData(
+  overrides?: Partial<FormValues>,
+): FormValues {
+  return {
+    name: "Rick",
+    age: 35,
+    email: "rick@example.com",
+    gender: "male",
+    terms: true,
+    password: "Test1@abc",
+    confirmPassword: "Test1@abc",
+    ...overrides,
+  };
+}
