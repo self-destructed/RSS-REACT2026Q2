f487523c2f59d312c898a39c3f866704ce9c8499
self-destructed <self.destructed.dan@gmail.com>
2026-06-08 13:53:36 +0300

    refactor: move hooks from shared/lib/hooks to shared/hook

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,8 +1,8 @@
 import { type JSX, useEffect, useRef } from "react";
 import Portal from "../portal/Portal";
-import { useFocusTrap } from "@shared/lib/hooks/use-focus-trap";
-import { useKeydown } from "@shared/lib/hooks/use-keydown";
-import { useBodyScroll } from "@shared/lib/hooks/use-body-scroll";
+import { useFocusTrap } from "@shared/hooks/use-focus-trap";
+import { useKeydown } from "@shared/hooks/use-keydown";
+import { useBodyScroll } from "@shared/hooks/use-body-scroll";
 
 interface ModalProps {
   open: boolean;
